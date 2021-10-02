46c3dfaee7b68ab840545fdf7ec12ed0fa265543
Diego Go <dgomes@Diegos-MacBook-Pro.local>
2021-10-02 19:00:03 -0300

    add more styles and effects

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { View, Text} from 'react-native';
+import { Container, SearchContainer, Input, SearchButton } from "./styles";
+import Header from "../../components/Header";
+import {Feather } from '@expo/vector-icons'; 
 
 function Home(){
     return(
 
-        <View>
-            <Text>Tela Home</Text>
-        </View>
+        <Container>
+            <Header title="React Filmes " />
+                <SearchContainer>
+                    <Input 
+                        placeholder="Homem Aranha"
+                        placeholderTextColor="#ddd"
+                    />
+                    <SearchButton>
+                        <Feather name="search" size={30} color="#fff" />
+                    </SearchButton>
+                </SearchContainer>
+        </Container>
     )
 }
 
