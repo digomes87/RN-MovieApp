ae05a16137acb970df6c87cbb1a54b2126578bea
Diego Go <dgomes@Diegos-MacBook-Pro.local>
2021-10-02 18:24:26 -0300

    add router, Stack and update color in menu

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 
 All here is basic but for learnig more about RN apps 
-Hoppe that you like something here, is my first steps, sorry 
+Hoppe that you like something here, is my first steps, sorry
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,58 @@
 import React from "react";
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import Home from "../pages/Home";
+import StackRoutes from "./stackRoutes";
 import Movie from "../pages/Movies";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 
 
 const Drawer = createDrawerNavigator();
 
-function Routes(){
-    return(
+function Routes() {
+    return (
         // create the routes
-        <Drawer.Navigator>
-            <Drawer.Screen name="Home" component={Home} />
-            <Drawer.Screen name="Movie" component={Movie} />
+        <Drawer.Navigator
+            screenOptions={{
+                heeaderShow: false,
+                drawerStyle: {
+                    backgroundColor: '#090A0E',
+                    paddingTop: 20,
+                },
+
+                drawerActiveBackgroundColor: '#E72F49',
+                drawerActiveTintColor: '#fff',
+                drawerInactiveTintColor: '#fff',
+            }}
+        >
+            <Drawer.Screen
+                name="HomeDrawer"
+                component={StackRoutes}
+                options={{
+                    title: 'Home',
+                    drawerIcon: ({ focused, size, color }) => (
+                        <MaterialCommunityIcons
+                            name={focused ? 'movie-open' : 'movie-outline'}
+                            size={size}
+                            color={color}
+                        />
+                    )
+                }}
+            />
+            <Drawer.Screen
+                name="Movie"
+                component={Movie}
+                options={{
+                    title: 'Movie',
+
+                    drawerIcon: ({ focused, size, color }) => (
+                        <MaterialCommunityIcons 
+                            name={focused ? 'archive' : 'archive-outline'}
+                            size={size}
+                            color={color}
+                        />
+                    )
+                }}
+            />
         </Drawer.Navigator>
     )
 }
